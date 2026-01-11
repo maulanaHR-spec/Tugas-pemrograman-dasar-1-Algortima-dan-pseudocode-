a98112688148b20fe89d6636e68008e225c9d414
maulanaHR-spec <maulana.rtg15@gmail.com>
2026-01-11 08:47:59 +0700

    Add files via upload

---
+++ #bilangan positif negatif atau nol.py
@@ -0,0 +1,9 @@
+#bilangan positif negatif atau nol
+bilangan = float(input("Masukkan sebuah bilangan: "))
+
+if bilangan > 0:
+    print("Bilangan Positif")
+elif bilangan < 0:
+    print("Bilangan Negatif")
+else:
+    print("Bilangan Nol")
---
+++ Program menghitung luas segitiga.py
@@ -0,0 +1,8 @@
+# Program menghitung luas segitiga
+
+alas = float(input("Masukkan alas segitiga: "))
+tinggi = float(input("Masukkan tinggi segitiga: "))
+
+luas = 0.5 * alas * tinggi
+
+print("Luas segitiga adalah:", luas)
